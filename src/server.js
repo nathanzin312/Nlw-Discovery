67fc7b658f7b769c868974cfcbd2d5b417e5ba27
Nathan <nathansanches890@gmail.com>
2022-05-28 20:55:53 -0300

    10 commit backend

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const route = require('./route');
+
+const server = express();
+
+server.set('view engine' , 'ejs');
+
+server.use(route);
+
+server.listen(3000, () => console.log("RODANDO"));
